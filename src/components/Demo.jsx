548071f7422c7762cf84c07cc50771737b51d9af
KevinThomas <128711174+KevinTHOMAS-Dev@users.noreply.github.com>
2023-09-15 22:06:06 +0200

    add: reduxtoolkit update: component demo and hero

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React from "react";
+
+import { copy, linkIcon, loader, tick } from "../Assets";
+import { useState } from "react";
 
 const Demo = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    
+    const [ article, setArticle ] = useState({
+        url: "",
+        summary: "",
+    })
+
+    const handleSubmit = async (e) => {
+        e.preventDefault;
+        alert("Submitted");
+    }
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <form
+        className="relative flex justify-center items-center"
+        onSubmit={() => {handleSubmit}}
+      >
+        <img
+          src={linkIcon}
+          alt="link_icon"
+          className="absolute left-0 my-2 w-5 "
+        />
+        <input
+          type="url"
+          placeholder="Enter a Url"
+          value={article.url}
+          onChange={(e) => {setArticle({
+            ...article, url: e.target.value
+          })}}
+          required
+          className="url_input peer"
+        />
+        <button
+          type="Submit"
+          className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+        >
+          ↵
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default Demo;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React from "react";
 
-import { logo } from "../assets"
+import { logo } from "../Assets";
 
 const Hero = () => {
-    return (
-        <header className="w-full flex justify-center items-center flex-col">
-            <nav className="flex justify-between items-center flex-col">
-                <img src={logo} alt="sumz_logo" className='w-28 object-contain'/>
-                <button type='button' onClick={() => {window.open("https://github.com/KevinTHOMAS-Dev")}} className="black_btn">Github</button>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center flex-col w-full mb-10 pt-3 ">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => {
+            window.open("https://github.com/KevinTHOMAS-Dev");
+          }}
+          className="black_btn"
+        >
+          Github
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">Open AI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
 };
 
 export default Hero;
